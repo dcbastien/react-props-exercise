8b64c3f176de11e87c0d244075c3a494eb3e0eee
dcbastien <ddcbastien@gmail.com>
2021-11-12 19:13:06 -0600

    Completed exercise with Bonus

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+//2b
+import Bonus from "./bonus/Bonus";
+import Exercise from "./exercise/Exercise";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    //2c
+    <div>
+    <Exercise/>
+    {/* 3a */}
+    <Exercise good="Awesome" bad="Terrible" />
+    <Bonus/>
     </div>
   );
 }
---
+++ src/exercise/Exercise.js
@@ -0,0 +1,16 @@
+
+
+//1a
+function Exercise({good=`Great`, bad=`Not Great`}) {
+    //2a
+    return (
+        <div>
+        <h1>{good}</h1>
+        <h1>{bad}</h1>
+        </div>
+    );
+}
+    
+
+ //1b   
+export default Exercise;
